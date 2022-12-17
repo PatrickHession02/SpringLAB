4bfe459c2072785f3f2ab3ae7c98ea7997eee410
PATRICK HESSION <G00394619@gmit.ie>
2022-12-17 03:34:44 +0000

    2nd Commit maven

--- src/main/java/PassengerController.java
+++ src/main/java/PassengerController.java
@@ -4,3 +4,7 @@ List<Passenger> myPassengers = List.of(
         new Passenger("Mr", "Paul", "123", 2L, 23),
         new Passenger("Mr", "Paul2", "6723", 2444L, 93),
         new Passenger("Mr", "Paul3", "144", 5562L, 53));
+
+
+@GetMapping("/{passengerID}")
+   new Passenger("Mr", "Paul3", "144", 5562L, 53));
