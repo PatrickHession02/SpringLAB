1a77d0bd075f78abd5653f2560b5c8b19ee45f0f
PATRICK HESSION <G00394619@gmit.ie>
2022-12-18 01:10:30 +0000

    Fixing code up

--- src/main/java/ie/atu/SpringLAB/PassengerData/main/java/PassengerService.java
+++ src/main/java/ie/atu/SpringLAB/PassengerData/main/java/PassengerService.java
@@ -1,18 +1,19 @@
 package ie.atu.SpringLAB.PassengerData.main.java;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 @Service
 
     public class PassengerService{
-        public List<Passenger> myPassengers = List.of(
-                new Passenger("Mr", "Paul", "123", 2L, 23),
-                new Passenger("Mr", "Paul2", "6723", 2444L, 93),
-                new Passenger("Mr", "Paul3", "144", 5562L, 53));
+    public List <Passenger> getPassengers()
+    {
+        List<Passenger> myPassengers = List.of(
+                new Passenger("Mr", "Paul", 12, 2L, 23),
+                new Passenger("Mr", "Paul2", 66, 2444L, 93),
+                new Passenger("Mr", "Paul3", 14, 5562L, 53));
 
-        return myPassengers();
+        return myPassengers;
     }
 
     public Passenger getPassenger( String passengerID)
